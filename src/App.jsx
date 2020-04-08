c9ab2d1970815c84bf13b4d2c828d4d3199edd0f
Kytra Murphree <kytramurphree@kytras-mbp.attlocal.net>
2020-04-08 13:07:45 -0500

    Initial Boiler Plate Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+
+    return (
+        <div className={'main'}>
+           THIS IS PLACEHOLDER TEXT
+        </div>
+    );
+
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
